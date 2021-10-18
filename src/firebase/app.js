8b34a06dd51006a4a2ced05a2ac13aba7f7216ca
KritapasSuwannawin <kritapas.ms@gmail.com>
2021-10-18 15:40:47 +0700

    fix firebase analytics

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -9,8 +9,10 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
 
+export const analytics = firebase.analytics();
 export default firebase;
