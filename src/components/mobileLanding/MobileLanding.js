12a7f9fe185f0d57bcced2f0fe6eadcef809c5bd
KritapasSuwannawin <kritapas.ms@gmail.com>
2021-10-21 09:26:40 +0700

    re-style mobile landing page

--- src/components/mobileLanding/MobileLanding.js
+++ src/components/mobileLanding/MobileLanding.js
@@ -1,14 +1,20 @@
 import './MobileLanding.scss';
 
 import logo50 from '../../svg/50px/Checkpoint with text 50px.svg';
+import imageUrl from './mobileBackground.png';
 
 function MobileLanding(props) {
   return (
     <div className="mobile-landing">
-      <img src={logo50} alt="" className="mobile-landing__logo"></img>
-      <p className="mobile-landing__heading">Coming Soon</p>
-      <p className="mobile-landing__paragraph">Our website is only available for computer users during a beta test.</p>
-      <p className="mobile-landing__paragraph">A mobile application of Checkpoint is coming soon.</p>
+      <div className="mobile-landing__content">
+        <img src={imageUrl} alt="" className="mobile-landing__content--background"></img>
+        <img src={logo50} alt="" className="mobile-landing__content--logo"></img>
+        <p className="mobile-landing__content--heading">Coming Soon</p>
+        <p className="mobile-landing__content--paragraph">
+          Our website is only available for computer users during a beta test.
+        </p>
+        <p className="mobile-landing__content--paragraph">A mobile application of Checkpoint is coming soon.</p>
+      </div>
     </div>
   );
 }
