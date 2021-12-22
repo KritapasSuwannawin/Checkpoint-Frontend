7fb1b88478a6bbf04c50553a4fcb8827732e1d75
KritapasSuwannawin <kritapas.ms@gmail.com>
2021-12-22 12:23:43 +0700

    separate login to sign up and sign in

--- src/components/loginPopup/LoginPopup.js
+++ src/components/loginPopup/LoginPopup.js
@@ -10,21 +10,94 @@ import { musicActions } from '../../store/musicSlice';
 function LoginPopup(props) {
   const dispatch = useDispatch();
 
+  const [signingUp, setSigningUp] = useState(true);
+
   const [invalidEmail, setInvalidEmail] = useState(false);
   const [invalidPassword, setInvalidPassword] = useState(false);
-  const [errorDuringAuthen, setErrorDuringAuthen] = useState(false);
+
   const [passwordNotMatch, setPasswordNotMatch] = useState(false);
+  const [errorDuringAuthen, setErrorDuringAuthen] = useState(false);
+  const [incorrectPassword, setIncorrectPassword] = useState(false);
+  const [accountNotExist, setAccountNotExist] = useState(false);
+  const [accountAlreadyExist, setAccountAlreadyExist] = useState(false);
+  const [agreeToPolicy, setAgreeToPolicy] = useState(undefined);
 
   const emailRef = useRef();
-  const passwordRef = useRef();
+  const passwordRef1 = useRef();
+  const passwordRef2 = useRef();
+  const checkboxRef1 = useRef();
+  const checkboxRef2 = useRef();
 
   function closeHandler() {
     props.closeHandler();
   }
 
-  function submitHandler() {
+  function signUpSubmitHandler() {
     const email = emailRef.current.value;
-    const password = passwordRef.current.value;
+    const password = passwordRef1.current.value;
+    const confirmPassword = passwordRef2.current.value;
+
+    const invalidEmail = !email.includes('@');
+    const invalidPassword = password.length < 6;
+    const passwordNotMatch = password !== confirmPassword;
+    const agreeToPolicy = checkboxRef1.current.checked;
+
+    if (invalidEmail) {
+      setInvalidEmail(true);
+    } else {
+      setInvalidEmail(false);
+    }
+
+    if (invalidPassword) {
+      setInvalidPassword(true);
+    } else {
+      setInvalidPassword(false);
+    }
+
+    if (passwordNotMatch) {
+      setPasswordNotMatch(true);
+    } else {
+      setPasswordNotMatch(false);
+    }
+
+    if (agreeToPolicy) {
+      setAgreeToPolicy(true);
+    } else {
+      setAgreeToPolicy(false);
+    }
+
+    if (!invalidEmail && !invalidPassword && !passwordNotMatch && agreeToPolicy) {
+      const data = { email, password, loginMethod: 'email', receiveNews: checkboxRef2.current.checked };
+
+      fetch(`${process.env.REACT_APP_BACKEND_URL}/api/member/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          const errorMessage = result.message;
+
+          setErrorDuringAuthen(errorMessage === 'error during authentication');
+          setAccountAlreadyExist(errorMessage === 'account already exist');
+
+          if (!errorMessage) {
+            const data = result.data[0];
+            dispatch(memberActions.setMember(data));
+            props.closeHandler();
+          }
+        })
+        .catch(() => {
+          setErrorDuringAuthen(true);
+        });
+    }
+  }
+
+  function signInSubmitHandler() {
+    const email = emailRef.current.value;
+    const password = passwordRef1.current.value;
 
     const invalidEmail = !email.includes('@');
     const invalidPassword = password.length < 6;
@@ -44,7 +117,7 @@ function LoginPopup(props) {
     if (!invalidEmail && !invalidPassword) {
       const data = { email, password, loginMethod: 'email' };
 
-      fetch(`${process.env.REACT_APP_BACKEND_URL}/api/member/login`, {
+      fetch(`${process.env.REACT_APP_BACKEND_URL}/api/member/signin`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -56,16 +129,17 @@ function LoginPopup(props) {
           const errorMessage = result.message;
 
           setErrorDuringAuthen(errorMessage === 'error during authentication');
-          setPasswordNotMatch(errorMessage === 'invalid password');
+          setIncorrectPassword(errorMessage === 'incorrect password');
+          setAccountNotExist(errorMessage === 'account not exist');
 
           if (!errorMessage) {
             const data = result.data[0];
             dispatch(memberActions.setMember(data));
-            data.backgroundId && dispatch(backgroundActions.changeBackgroundHandler({ id: data.backgroundId }));
-            data.musicId && dispatch(musicActions.setInitialMusic(data.musicId));
-            data.musicCategory && dispatch(musicActions.setMusicCategory(data.musicCategory));
-            data.favouriteMusicIdArr && dispatch(musicActions.setFavouriteMusicIdArr(data.favouriteMusicIdArr));
-            data.playFromPlaylist !== undefined && dispatch(musicActions.setPlayFromPlaylist(data.playFromPlaylist));
+            dispatch(backgroundActions.changeBackgroundHandler({ id: data.backgroundId }));
+            dispatch(musicActions.setInitialMusic(data.musicId));
+            dispatch(musicActions.setMusicCategory(data.musicCategory));
+            dispatch(musicActions.setFavouriteMusicIdArr(data.favouriteMusicIdArr));
+            dispatch(musicActions.setPlayFromPlaylist(data.playFromPlaylist));
             props.closeHandler();
           }
         })
@@ -75,25 +149,72 @@ function LoginPopup(props) {
     }
   }
 
+  function signUpClickHandler() {
+    setSigningUp(this);
+
+    setInvalidEmail(false);
+    setInvalidPassword(false);
+
+    setPasswordNotMatch(false);
+    setErrorDuringAuthen(false);
+    setIncorrectPassword(false);
+    setAccountNotExist(false);
+    setAccountAlreadyExist(false);
+    setAgreeToPolicy(undefined);
+  }
+
   return (
     <div className="login-popup">
       <form>
         <div className="login-popup__close-btn" onClick={closeHandler}></div>
-        <p className="login-popup__title">Become a member!</p>
-        <div className="login-popup__input-section">
-          <label>Email</label>
-          <input type="text" placeholder="email" id="email" ref={emailRef}></input>
+        <div className="login-popup__title-container">
+          <p
+            className={`login-popup__title ${!signingUp ? 'not-current' : ''}`}
+            onClick={signUpClickHandler.bind(true)}
+          >
+            Sign up
+          </p>
+          <p
+            className={`login-popup__title ${signingUp ? 'not-current' : ''}`}
+            onClick={signUpClickHandler.bind(false)}
+          >
+            Sign in
+          </p>
         </div>
+        <input className="login-popup__input" type="text" placeholder="Email" id="email" ref={emailRef}></input>
         {invalidEmail && <p className="login-popup__error-msg">Invalid email</p>}
-        <div className="login-popup__input-section">
-          <label>Password</label>
-          <input type="password" placeholder="password" ref={passwordRef}></input>
-        </div>
+        <input className="login-popup__input" type="password" placeholder="Password" ref={passwordRef1}></input>
         {invalidPassword && <p className="login-popup__error-msg">Password must contain at least 6 characters</p>}
-        {passwordNotMatch && <p className="login-popup__error-msg">Invalid password</p>}
-        <div className="login-popup__submit-btn" onClick={submitHandler}>
-          Join
+        {!signingUp && (
+          <>
+            {incorrectPassword && <p className="login-popup__error-msg">Incorect password</p>}
+            {accountNotExist && <p className="login-popup__error-msg">Account does not exist, please sign up first</p>}
+          </>
+        )}
+        {signingUp && (
+          <>
+            <input
+              className="login-popup__input"
+              type="password"
+              placeholder="Confirm password"
+              ref={passwordRef2}
+            ></input>
+            {passwordNotMatch && <p className="login-popup__error-msg">Passwords do not match</p>}
+            {accountAlreadyExist && <p className="login-popup__error-msg">Account already exists, please sign in</p>}
+            <div className="login-popup__privacy-container margin-top">
+              <input type="checkbox" ref={checkboxRef1}></input>
+              <p>By registering, you agree to the Terms and Privacy Policy.</p>
+            </div>
+            <div className="login-popup__privacy-container">
+              <input type="checkbox" ref={checkboxRef2}></input>
+              <p>I agree to receive news and updates from Checkpoint.</p>
+            </div>
+          </>
+        )}
+        <div className="login-popup__submit-btn" onClick={signingUp ? signUpSubmitHandler : signInSubmitHandler}>
+          {signingUp ? 'Sign up' : 'Sign in'}
         </div>
+        {signingUp && agreeToPolicy === false && <p className="login-popup__error-msg">Please agree to the policy</p>}
         {errorDuringAuthen && <p className="login-popup__error-msg">Error occured, please try again later</p>}
       </form>
     </div>
