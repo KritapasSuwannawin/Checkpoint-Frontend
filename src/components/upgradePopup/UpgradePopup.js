7156643626c25253fa22101f412897b0fe58a8de
KritapasSuwannawin <kritapas.ms@gmail.com>
2022-01-12 23:47:39 +0700

    implement omise payment

--- src/components/upgradePopup/UpgradePopup.js
+++ src/components/upgradePopup/UpgradePopup.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Script from 'react-load-script';
 
 import './UpgradePopup.scss';
 
@@ -8,6 +9,7 @@ import { memberActions } from '../../store/memberSlice';
 function UpgradePopup(props) {
   const dispatch = useDispatch();
   const memberId = useSelector((store) => store.member.memberId);
+  const email = useSelector((store) => store.member.email);
 
   const [errorDuringUpgrading, setErrorDuringUpgrading] = useState(false);
 
@@ -15,10 +17,9 @@ function UpgradePopup(props) {
     props.closeHandler();
   }
 
-  function submitHandler() {
-    const data = { memberId };
-
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/api/member/upgrade`, {
+  function createCreditCardCharge(token) {
+    const data = { memberId, email, token };
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/api/member/payment`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -27,11 +28,11 @@ function UpgradePopup(props) {
     })
       .then((response) => response.json())
       .then((result) => {
-        if (result.message) {
-          setErrorDuringUpgrading(true);
-        } else {
+        if (result.active) {
           props.closeHandler();
           dispatch(memberActions.upgradeMember());
+        } else {
+          setErrorDuringUpgrading(true);
         }
       })
       .catch(() => {
@@ -39,15 +40,50 @@ function UpgradePopup(props) {
       });
   }
 
+  function handleScriptLoad() {
+    window.OmiseCard.configure({
+      publicKey: process.env.REACT_APP_OMISE_PUBLIC_KEY,
+      frameLabel: 'Checkpoint',
+      submitLabel: 'Subscribe for',
+      currency: 'usd',
+    });
+  }
+
+  function creditCardConfigure() {
+    window.OmiseCard.configure({
+      defaultPaymentMethod: 'credit_card',
+      otherPaymentMethods: [],
+    });
+    window.OmiseCard.configureButton('#credit-card');
+    window.OmiseCard.attach();
+  }
+
+  function omiseCardHandler() {
+    window.OmiseCard.open({
+      amount: 399,
+      onCreateTokenSuccess: (token) => {
+        createCreditCardCharge(token);
+      },
+      onFormClosed: () => {},
+    });
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    creditCardConfigure();
+    omiseCardHandler();
+  }
+
   return (
     <div className="upgrade-popup">
+      <Script url="https://cdn.omise.co/omise.js" onLoad={handleScriptLoad} />
       <form>
         <div className="upgrade-popup__close-btn" onClick={closeHandler}></div>
         <p className="upgrade-popup__title">Upgrade to premium!</p>
         <p className="upgrade-popup__content">3.99$ / month</p>
-        <div className="upgrade-popup__submit-btn" onClick={submitHandler}>
+        <button id="credit-card" className="upgrade-popup__submit-btn" type="button" onClick={handleClick}>
           Upgrade
-        </div>
+        </button>
         {errorDuringUpgrading && <p className="upgrade-popup__error-msg">Error occured, please try again later</p>}
       </form>
     </div>
